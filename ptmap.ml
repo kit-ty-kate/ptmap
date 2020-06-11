5316fba0e055d86c2f557b57b4ac1e358f853392
Kate <kit.ty.kate@disroot.org>
2020-06-11 20:38:31 +0100

    OCaml 4.11 support

--- ptmap.ml
+++ ptmap.ml
@@ -188,6 +188,11 @@ let rec filter pr = function
   | Leaf (k, v) as t -> if pr k v then t else Empty
   | Branch (p,m,t0,t1) -> branch (p, m, filter pr t0, filter pr t1)
 
+let rec filter_map f = function
+  | Empty -> Empty
+  | Leaf (k, v) -> (match f k v with Some v -> Leaf (k, v) | None -> Empty)
+  | Branch (p,m,t0,t1) -> branch (p, m, filter_map f t0, filter_map f t1)
+
 let partition p s =
   let rec part (t,f as acc) = function
     | Empty -> acc
